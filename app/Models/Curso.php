75cf189d3048b19bbcf8eeace14cadd47fb4a4cb
Jorge Vega <j.vega@mafasa.org>
2023-06-22 10:55:56 -0700

    Introduccion a Eloquent

---
+++ app/Models/Curso.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    // Aqui se puede ignorar la convencion y especificar la tabla que el modelo afectara
+    //protected $table = "users";
+
+}
